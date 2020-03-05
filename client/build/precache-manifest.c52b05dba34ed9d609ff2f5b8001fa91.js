57ba977983f9d7aebf32b509436998eb6ec1bcb8
nikostzam <nikosczt@gmail.com>
2020-03-05 22:36:48 +0200

    'first'

---
+++ client/build/precache-manifest.c52b05dba34ed9d609ff2f5b8001fa91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966ceb4b3b37eca181909f993465ab04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cb3c8b8bf6933c9bc9",
+    "url": "/static/css/main.be61e791.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6cb3c8b8bf6933c9bc9",
+    "url": "/static/js/main.1a8a7cde.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  },
+  {
+    "revision": "5027a761fbd05ab968ececd0b739177d",
+    "url": "/static/media/bg.5027a761.jpg"
+  }
+]);
