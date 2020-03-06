ed04c1e1d1ca8c13cd37d956a29afa0264537d08
nikostzam <nikosczt@gmail.com>
2020-03-06 23:01:16 +0200

    '3'

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if(process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
- app.use('/', express.static(path.join(__dirname, '/client/build')));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
